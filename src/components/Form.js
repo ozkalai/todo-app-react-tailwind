b29fc58fad4850d6872e023b924cb626fd29a1c8
ozkalai <melihozkalayy@gmail.com>
2021-04-08 15:35:39 +0300

    updated all structure

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,37 @@
-import React from 'react'
+import React from "react";
 
-function Form({inputText ,setInputText , setTodos ,todos}) {
-    const clickHandler = (e) => {
-        e.preventDefault();
+function Form({ inputText, setInputText, setTodos, todos }) {
+  const clickHandler = (e) => {
+    e.preventDefault();
     setTodos([
-      { text: inputText, completed: false, id: Math.ceil(Math.random() * 1000) },...todos
+      {
+        text: inputText,
+        completed: false,
+        id: Math.ceil(Math.random() * 1000),
+      },
+      ...todos,
     ]);
     setInputText("");
   };
-    const inputTextHandler = (e) => {
-        setInputText(e.target.value)
-    }
-    return (
-        <div className="flex justify-center items-center bg-white  mx-auto rounded-2xl border-gray-100 border-2 ">
-            <form className="">
-                <input
-                className="w-full box-border outline-none bg-transparent text-gray-50 placeholder-gray-300 text-2xl ml-2 p-2 flex"
-                placeholder="New"
-                onChange={inputTextHandler}
-                value={inputText}
-                type="text"
-                />
-            </form>
-            <i onClick={clickHandler} className="fas fa-thumbtack text-primary mr-4 text-lg"></i>
-
-            
-        </div>
-    )
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <div className="flex justify-center w-full mb-6 pb-6 border-b-2 border-gray-100 items-center bg-white  mx-auto ">
+      <form className="flex w-full  rounded-2xl border-gray-100 border-2 mx-2">
+        <input
+          className="w-full box-border outline-none bg-transparent text-gray-600 placeholder-gray-300 text-2xl ml-2 p-4 flex"
+          placeholder="New"
+          onChange={inputTextHandler}
+          value={inputText}
+          type="text"
+        />
+        <button onClick={clickHandler}>
+          <i className="fas fa-thumbtack text-blue-500 mr-4 text-lg outline-none"></i>
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function Todo({text ,todo}) {
-    return (
-        <div className="flex w-full h-auto bg-white my-1.5 p-4">
-            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
-            {text}
-            </li>
-            <button className="complete-btn">
-                <i className="fas fa-check"></i>
-            </button>
-            <button className="trash-btn">
-                <i className="fas fa-trash"></i>
-            </button>
-        </div>
-    )
+function Todo({ text, todo }) {
+  return (
+    <div className="flex w-full h-auto bg-white my-1.5 p-4 bg-gray-100 flex justify-between px-4 font-sans ">
+      <li
+        className={`todo-item ${
+          todo.completed ? "completed" : ""
+        } text-gray-400 text-2xl`}
+      >
+        {text}
+      </li>
+      <div className="icons flex mx-2 text-xl">
+        <button className="complete-btn text-green-500 mx-2">
+          <i className="fas fa-check"></i>
+        </button>
+        <button className="trash-btn text-red-500">
+          <i className="fas fa-trash"></i>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
